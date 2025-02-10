62934e5d8c4aef594f0d2f7c831475153b19add6
PercyGoss <gossa0902@student.faytechcc.edu>
2025-02-09 23:19:03 -0500

    Add files via upload

---
+++ Percy Goss - P1LAB1.py
@@ -0,0 +1,8 @@
+# Percy Goss
+# 09/02/2025 (9th of February, 2025)
+# P1LAB1
+# A simple introductory assignment for Python. 2 inputs (first and last name) and an output that uses the information given to print a "personalized" message.
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print("Hello, " + fname + " " + lname + "! Welcome to CTI-110!")
