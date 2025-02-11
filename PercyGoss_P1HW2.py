b3f6a0a4ea619d243fca30c19d4ab84aa009c962
PercyGoss <gossa0902@student.faytechcc.edu>
2025-02-11 14:10:39 -0500

    Add files via upload

---
+++ PercyGoss_P1HW2.py
@@ -0,0 +1,27 @@
+# Percy
+# 11/02/2025 (11th of February, 2025)
+# P1HW1
+# Another simple demonstration of Python's formula system, differently formatted.
+
+print("This program calculates and displays your hypothetical travel expenses based on your own estimates.")
+print()
+dest = input("Where are you going? ")
+print()
+budget = int(input("What is your budget? "))
+print()
+gas = int(input("How much do you think you'll spend on gas? "))
+print()
+stay = int(input("And about how much do you think you'll need to spend on accomodations or a hotel? "))
+print()
+food = int(input("Lastly, how much can you assume you'll need for food? "))
+expenses = gas+stay+food
+print()
+print("----------Travel Expenses----------")
+print("Location:", dest)
+print("Initial Budget:", budget)
+print()
+print("Fuel:", gas)
+print("Accomodation:", stay)
+print("Food:", food)
+print()
+print("Remaining Balance:", budget-expenses)
